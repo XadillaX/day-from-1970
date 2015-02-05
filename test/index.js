43e4804cf2d65c17026ffa63a1b6c816765ab317
XadillaX <admin@xcoder.in>
2015-02-05 11:30:44 +0800

    edit test

--- test/index.js
+++ test/index.js
@@ -3,11 +3,27 @@
  */
 var df1970 = require("../");
 
-var t = 1396946977;
-var list = [];
+var t = 1420041600;
+var list1 = [];
+var list2 = [];
 for(var i = 0; i < 10; i++) {
-    list.push(df1970(t));
+    list1.push(df1970(t, 0));
+    list2.push(df1970(t));
     t += 86400;
 }
 
-console.log(list);
+console.log(list1);
+console.log(list2);
+
+var t = 57600;
+var list3 = [];
+var list4 = [];
+for(var i = 0; i < 10; i++) {
+    list3.push(df1970(t, 0));
+    list4.push(df1970(t));
+    t += 86400;
+}
+
+console.log(list3);
+console.log(list4);
+
