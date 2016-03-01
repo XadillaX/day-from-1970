9f4d41b7eb1ef1ee1c9649237845632581c754ed
XadillaX <admin@xcoder.in>
2016-03-01 18:01:16 +0800

    update a test case

--- test/index.js
+++ test/index.js
@@ -6,14 +6,14 @@ var moment = require("moment");
 var df = require("../");
 
 describe("Test", function() {
-    it("should finish a loop", function() {
+    it("should finish a loop in UTC", function() {
         this.timeout(20000);
-        var current = 23934;
+        var current = 80000;
         var day = 1;
         var week = 1;
         var month = 1;
         var year = 1;
-        var cm = moment.unix(current);
+        var cm = moment.unix(current).utc();
         var pm = null;
 
         while(cm.year() !== 2222) {
@@ -27,7 +27,7 @@ describe("Test", function() {
 
             current += 86400;
             pm = cm;
-            cm = moment.unix(current);
+            cm = moment.unix(current).utc();
             if(cm.year() !== pm.year()) {
                 year++;
                 month++;
@@ -45,4 +45,45 @@ describe("Test", function() {
             }
         }
     });
+
+    it("should finish a loop in +8", function() {
+        this.timeout(20000);
+        var current = 80000;
+        var day = 2;
+        var week = 1;
+        var month = 1;
+        var year = 1;
+        var cm = moment.unix(current).utc(8);
+        var pm = null;
+
+        while(cm.year() !== 2222) {
+            var tmp = df(cm.unix(), 8);
+
+            // console.log(tmp, current, cm.format());
+            tmp.day.should.be.eql(day);
+            tmp.week.should.be.eql(week);
+            tmp.month.should.be.eql(month);
+            tmp.year.should.be.eql(year);
+
+            current += 86400;
+            pm = cm;
+            cm = moment.unix(current).utc(8);
+            if(cm.year() !== pm.year()) {
+                year++;
+                month++;
+                if(cm.day() === 0) week++;
+                day++;
+            } else if(cm.month() !== pm.month()) {
+                month++;
+                if(cm.day() === 0) week++;
+                day++;
+            } else if(cm.week() !== pm.week()) {
+                week++;
+                day++;
+            } else {
+                day++;
+            }
+        }
+    });
+
 });
